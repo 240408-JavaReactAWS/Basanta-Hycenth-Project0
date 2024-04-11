d714ddf6a253378c070881c203293a20af7d1d13
sapkotabasanta10 <sapkotabasanta10@gmail.com>
2024-04-11 18:42:26 -0400

    initial commit

---
+++ FurnitureCollection/src/main/java/com/revature/FurnitureCollection/FurnitureCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.revature.FurnitureCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureCollectionApplication.class, args);
+	}
+
+}
